b6079ad3ad0e697b7864a98488691b46ff90aafe
Axel <axel.canneroth1@gmail.com>
2023-01-12 21:13:08 +0100

    ta bort hej

--- src/components/search/GameCollectionList.js
+++ src/components/search/GameCollectionList.js
@@ -14,7 +14,6 @@ const addToDashboard = () => {
    //  props.setGames(props.gamesToAdd)
      addGame(props.gamesToAdd);
      props.setGamesToAdd({games: []})
-     console.log('hej')
 };
            
   return (
